ada94d107142b255b5f55bf6a6aae515a761b3b6
geomio <geoff.millner22@gmail.com>
2021-03-29 09:48:35 -0700

    made html pages for js html csharp and a quiz page

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// let answer = question + question1 ect
+
+const jscript = 25
+const html = 40
+const csharp = 66
+
+let answer = jscript + html + csharp;
+
+if(answer <= 33 && answer >= 0){
+  console.log("test working");
+}else if(answer <= 66 && answer >= 34){
+  console.log("nexttest2");
+}else if(answer <= 100 && answer >= 67){
+  console.log("thirdtest");
+}else if(answer < 100);{
+  console.log("failsafetohigh");
+}
