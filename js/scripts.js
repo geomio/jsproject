6736fb100364a497b01c6cbd7f1fd42a73ba7ab4
geomio <geoff.millner22@gmail.com>
2021-03-31 10:16:41 -0700

    removed commit area in js and added page sections in css via commit message

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,24 @@
-// $(document).ready(function() {
-//   $("form#quiz").submit(function(event) {
-//     const question1 = parseInt($("#question1").val());
-//     const question2 = parseInt($("#question2").val());
-//     const question3 = parseInt($("#question3").val());
-//     const question4 = parseInt($("#question4").val());
-//     const question5 = parseInt($("#question5").val());
-//     $("#answer1").hide();
-//     $("#answer2").hide();
-//     $("#answer3").hide();
-//            
-//     let answer = question1 + question2 + question3 + question4 + question5
-//     event.preventDefault();
-//   });
-// });
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    const question1 = parseInt($("#question1").val());
+    const question2 = parseInt($("#question2").val());
+    const question3 = parseInt($("#question3").val());
+    const question4 = parseInt($("#question4").val());
+    const question5 = parseInt($("#question5").val());
+    $("#answer1").hide();
+    $("#answer2").hide();
+    $("#answer3").hide();
+           
+    let answer = question1 + question2 + question3 + question4 + question5
 
-$("#answer1").hide();
-$("#answer2").hide();
-$("#answer3").hide();
+    if(answer <= 33 && answer >= 0){
+      $("#answer1").show();
+    }else if(answer <= 66 && answer >= 34){
+      $("#answer2").show();
+    }else if(answer <= 100 && answer >= 67){
+      $("#answer3").show();
+    }
 
-
-const jscript = 25
-const html = 40
-const csharp = 66
-
-let answer = jscript + html + csharp;
-
-if(answer <= 33 && answer >= 0){
-  $("#answer1").show();
-}else if(answer <= 66 && answer >= 34){
-  $("#answer2").show();
-}else if(answer <= 100 && answer >= 67){
-  $("#answer3").show();
-}else if(answer < 100);{
-  $("#answer3").show();
-}
+    event.preventDefault();
+  });
+});
